a9e9317f0007240018df836bacbc1efd19709bb9
Ryan Miller <ryanmillergm@gmail.com>
2019-07-08 10:16:49 -0600

    Refactored food model to take in attributes

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,15 +1,13 @@
 class FoodsController < ApplicationController
   def index
-    binding.pry
     food = params[:q]
     @conn = Faraday.new(url: "https://api.nal.usda.gov") do |faraday|
       faraday.headers["FOOD_KEY"] = "mHcgZdb9IRFCjnYSQ4DkMbzvWzKjTBpWIt71HZCD"
       faraday.adapter Faraday.default_adapter
     end
 
-    response = @conn.get("ndb/search/?format=json&q=#{food}&sort=n&max=25&offset=0&api_key=FOOD_KEY ")
-
-    results = JSON.parse(response.body, symbolize_names: true)[:results]
+    response = @conn.get("ndb/search/?format=json&q=#{food}&sort=n&max=25&offset=0&api_key=mHcgZdb9IRFCjnYSQ4DkMbzvWzKjTBpWIt71HZCD")
+    results = JSON.parse(response.body, symbolize_names: true)
 
     @foods = results.map do |result|
       Food.new(result)
--- app/models/food.rb
+++ app/models/food.rb
@@ -1,7 +1,12 @@
 class Food
-  attr_reader :name
+  attr_reader :name,
+              :items,
+              :name
 
   def initialize(attributes = {})
-    @name = attributes[:name]
+    binding.pry
+    @total = attributes[1][:total]
+    @items = attributes[1][:item]
+    @name = attributes[1][:q]
   end
 end

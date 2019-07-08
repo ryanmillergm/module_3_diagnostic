7fa7f4532f0a68442a002553a6c0fedfa53288a3
Ryan Miller <ryanmillergm@gmail.com>
2019-07-08 09:13:49 -0600

    Initial feature test setup user_can_search_form

---
+++ spec/features/user_can_search_form_spec.rb
@@ -0,0 +1,24 @@
+require "rails_helper"
+
+feature "as a user" do
+  scenario "on the home page I can search form" do
+    visit "/"
+
+    fill_in "search", with: "sweet potatoes"
+
+    click_on "Search"
+
+    expect(current_path).to eq('/foods')
+    expect(page).to have_content("531 for sweet potatoes")
+  end
+end
+
+# As a user,
+# When I visit "/"
+# And I fill in the search form with "sweet potatoes"
+# (Note: Use the existing search form)
+# And I click "Search"
+# Then I should be on page "/foods"
+# Then I should see a total of the number of items
+# returned by the search. (531 for sweet potatoes)
+# Then I should see a list of ten foods sorted by relevance.
